1bc57d5f513f1fda0baed5681fbe1c10788eb062
me <reset475@gmail.com>
2025-02-09 16:59:10 +0700

    add react cra

---
+++ solutions/day-03/index.js
@@ -0,0 +1,5 @@
+import dosomething from 'math.js'
+
+import { addTwo,multiply,substract } from './math'
+import * as everything from 'math.js'
+console.log(addTwo(2,3))
---
+++ solutions/day-03/math.js
@@ -0,0 +1,10 @@
+export const addTwo = (a,b) => a + b
+export const multiply = (a,b) => a*b
+export const substract = (a,b) => a - b
+export default (function dosomething() {
+    return{
+        addTwo,
+        multiply,
+        substract
+    }
+})()
